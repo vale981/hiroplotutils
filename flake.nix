15117c51a1b4b80c1881fecf87b4b452eeff4a17
Valentin Boettcher <hiro@protagon.space>
2024-11-13 12:12:40 -0500

    make naming in flake.nix consistent

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
-        hiroPlotUtils = { poetry2nix, lib }: poetry2nix.mkPoetryApplication {
+        hiroplotutils = { poetry2nix, lib }: poetry2nix.mkPoetryApplication {
           projectDir = self;
           overrides = poetry2nix.overrides.withDefaults (final: super:
             lib.mapAttrs
@@ -35,13 +35,13 @@
           overlays = [
             poetry2nix.overlays.default
             (final: _: {
-              hiroPlotUtils = final.callPackage hiroPlotUtils { };
+              hiroplotutils = final.callPackage hiroplotutils { };
             })
           ];
         };
       in
       {
-        packages.default = pkgs.hiroPlotUtils;
+        packages.default = pkgs.hiroplotutils;
         devShells = {
           # Shell for app dependencies.
           #
